b6d0a44d9264e9dbe0ab919e0fa9c1c4ef2ab988
kentt <kentt8046@gmail.com>
2017-10-21 11:57:09 +0900

    Add test "createUser" without snapshot

--- src/__tests__/service.spec.ts
+++ src/__tests__/service.spec.ts
@@ -11,8 +11,26 @@ describe("service", () => {
       email: "test@email.com",
       accountName: "test",
     };
-    const user = service.createUser(input);
+    const { id, ...user } = service.createUser(input);
+    expect(user).toEqual({
+      firstName: "太郎",
+      lastName: "てすと",
+      email: "test@email.com",
+      accountName: "test",
+    });
+    expect(typeof id).toEqual("string");
+  });
+
+  it("createUser - snapshot", () => {
+    const input = {
+      firstName: "太郎",
+      lastName: "てすと",
+      email: "test@email.com",
+      accountName: "test",
+    };
+    const { id, ...user } = service.createUser(input);
     expect(user).toMatchSnapshot();
+    expect(typeof id).toEqual("string");
   });
 
 });
